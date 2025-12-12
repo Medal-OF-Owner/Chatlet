f2c5289fc0b7eb2ae172229f4a125e3d5dfb7ff3
Moi <nox874425@gmail.com>
2025-12-11 21:04:19 -0500

    Add normalizedNickname column to accounts table

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -86,6 +86,7 @@ async function runMigrations() {
         id SERIAL PRIMARY KEY,
         email VARCHAR(320) NOT NULL UNIQUE,
         nickname VARCHAR(100) NOT NULL UNIQUE,
+        "normalizedNickname" VARCHAR(100) NOT NULL UNIQUE,
         "passwordHash" VARCHAR(255) NOT NULL,
         "emailVerified" TIMESTAMP,
         "verificationToken" VARCHAR(255),
